b2b852a607322b908244b52fd462370ec81aba04
Peichun Hua <peichunhua@link.cuhk.edu.cn>
2026-01-09 23:23:41 +0800

    Update deprecated code

--- scripts/reproduce_experiments.py
+++ scripts/reproduce_experiments.py
@@ -8,11 +8,11 @@ experiment, trainer = analyze_utils.load_experiment_and_trainer_from_pretrained(
     "jxm/gtr__nq__32__correct"
 )
 train_datasets = experiment._load_train_dataset_uncached(
-    model=trainer.model, tokenizer=trainer.tokenizer, embedder_tokenizer=trainer.embedder_tokenizer
+    model=trainer.model, tokenizer=trainer.processing_class, embedder_tokenizer=trainer.embedder_tokenizer
 )
 
 val_datasets = experiment._load_val_datasets_uncached(
-    model=trainer.model, tokenizer=trainer.tokenizer, embedder_tokenizer=trainer.embedder_tokenizer
+    model=trainer.model, tokenizer=trainer.processing_class, embedder_tokenizer=trainer.embedder_tokenizer
 )
 trainer.args.per_device_eval_batch_size = 16
 trainer.sequence_beam_width = 1
